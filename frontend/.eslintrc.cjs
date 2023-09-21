84c536a73b5ec899121e986deae2087fb31f8e70
nazimuddin <nazimuddin102001@gmail.com>
2023-09-21 07:13:59 +0600

    modified in card reducer

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     "no-undef": "off",
     "react/prop-types": "off",
+    "no-case-declarations": "off",
     "no-unused-vars": "off",
     "react-hooks/exhaustive-deps": "off",
     "react-refresh/only-export-components": [
